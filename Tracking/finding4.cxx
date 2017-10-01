8859f5034faae48125b56855d208803e7d7f62d9
wuchen <wuchen1106@gmail.com>
2017-10-02 00:19:06 +0900

    finding4 under construction...

--- Tracking/finding4.cxx
+++ Tracking/finding4.cxx
@@ -33,12 +33,47 @@
 
 std::vector<int> * i_layerID = 0;
 std::vector<int> * i_wireID = 0;
+std::vector<double> * o_driftD = 0;
+std::vector<double> * o_dxl = 0;
+std::vector<double> * o_dxr = 0;
+
+//===================Chamber Parameter============================
+double U = 8; // mm
+double chamberHL = 599.17/2; // mm
+// map for wire position
+double  map_x[NLAY][NCEL][2];
+double  map_y[NLAY][NCEL][2];
+double  map_z[NLAY][NCEL][2];
+int     map_ch[NLAY][NCEL];
+int     map_bid[NLAY][NCEL];
+int     map_lid[NBRD][NCHS];
+int     map_wid[NBRD][NCHS];
+int     map_theta[NBRD][NCHS];
+// mcp for cross points
+double mcp_xc[NZXP][NCEL][NCEL]; // z-x planes corresponding to the layerID of the lower layer counting from 1 
+double mcp_zc[NZXP][NCEL][NCEL]; // z-x planes corresponding to the layerID of the lower layer counting from 1 
+
+//===================About tracking============================
+double yup = 623.97007;
+double ydown = 527.60011;
+double zup;
+double zdown;
+double xup;
+double xdown;
+double slx;
+double slz;
+TF1 * f_x = new TF1("f_x","pol1",500,640); // x VS y
+TGraph * g_x = 0; // x VS y
+TF1 * f_z = new TF1("f_z","pol1",500,640); // z VS y
+TGraph * g_z = 0; // z VS y
 
 //===================Functions============================
 void print_usage(char* prog_name);
 double t2x(double time, int lid, int wid, int lr, int & status);
-int ChooseHits(int ipick, std::vector<int> & v_pick_lid, std::vector<int> & v_pick_index, std::vector<std::vector<int> > & v_hit_index);
-int checkCrossPoints(std::vector<int> & v_pick_index);
+int ChooseHits(int ipick, std::vector<int> & v_pick_lid, std::vector<int> & pick_ihit, std::vector<double> & pick_wy, std::vector<std::vector<int> > & v_layer_ihit,int & iselection);
+int checkCrossPoints(int nPicks,std::vector<int> & pick_ihit, std::vector<double> & pick_wy);
+int getCrossPoints(int ihit,int jhit,int ilr,int jlr,double deltaY);
+int updateHitPositions(int nPicks, std::vector<int> & pick_ihit, std::vector<double> & pick_wy);
 int fityx();
 int fityz();
 
@@ -72,29 +107,25 @@ int main(int argc, char** argv){
     }
     int t0shift = 0;
     if (argc>=5) t0shift = (int)strtol(argv[4],NULL,10);
-    printf("t0shift = %d\n",t0shift);
-    int nEventMax = 0;
-    if (argc>=6) nEventMax = (int)strtol(argv[5],NULL,10);
+    int iEntryStart = 0;
+    int iEntryStop = 0;
+    if (argc>=6){
+        iEntryStop = (int)strtol(argv[5],NULL,10)-1;
+    }
+    if (argc>=7){
+        iEntryStart = (int)strtol(argv[5],NULL,10);
+        iEntryStop = (int)strtol(argv[6],NULL,10);
+    }
+    printf("runNo       = %d\n",runNo);
+    printf("test layer  = %d\n",testlayer);
+    printf("suffix      = %s\n",suffix.Data());
+    printf("t0shift     = %d\n",t0shift);
+    printf("Start Entry = %d\n",iEntryStart);
+    printf("Stop Entry  = %d\n",iEntryStop);
     double tmin = -50;
     double tmax = 380; // FIXME: need to be properly set
 
-    //===================Chamber Parameter============================
-    double U = 8; // mm
-    double chamberHL = 599.17/2; // mm
-
     //===================Prepare Maps============================
-    // map for wire position
-    double  map_x[NLAY][NCEL][2];
-    double  map_y[NLAY][NCEL][2];
-    double  map_z[NLAY][NCEL][2];
-    int     map_ch[NLAY][NCEL];
-    int     map_bid[NLAY][NCEL];
-    int     map_lid[NBRD][NCHS];
-    int     map_wid[NBRD][NCHS];
-    int     map_check[NLAY][NCEL];
-    // mcp for cross points
-    double mcp_xc[NZXP][NCEL][NCEL]; // z-x planes corresponding to the layerID of the lower layer counting from 1 
-    double mcp_zc[NZXP][NCEL][NCEL]; // z-x planes corresponding to the layerID of the lower layer counting from 1 
     for(int lid = 0; lid<NLAY; lid++){
         for (int wid = 0; wid<NCEL; wid++){
             map_x[lid][wid][0] = 0;
@@ -103,7 +134,6 @@ int main(int argc, char** argv){
             map_x[lid][wid][1] = 0;
             map_y[lid][wid][1] = 0;
             map_z[lid][wid][1] = 0;
-            map_check[lid][wid]=0;
             if (lid <NZXP){ // z-x planes corresponding to the layerID of the lower layer counting from 1 
                 for (int wjd = 0; wjd<NCEL; wjd++){
                     mcp_xc[lid][wid][wjd] = 999;
@@ -132,8 +162,6 @@ int main(int argc, char** argv){
     TTree_wirepos->SetBranchAddress("yhv",&wp_yhv);
     TTree_wirepos->SetBranchAddress("xro",&wp_xro);
     TTree_wirepos->SetBranchAddress("yro",&wp_yro);
-    std::vector<double> v_wire_xc; // to get a graph of wire centers
-    std::vector<double> v_wire_yc; // to get a graph of wire centers
     for (int i = 0; i<TTree_wirepos->GetEntries(); i++){
         TTree_wirepos->GetEntry(i);
         if (wp_lid>=0&&wp_lid<NLAY&&wp_wid>=0&&wp_wid<NCEL){
@@ -145,11 +173,7 @@ int main(int argc, char** argv){
             map_z[wp_lid][wp_wid][1] = chamberHL;
             map_ch[wp_lid][wp_wid] = wp_ch;
             map_bid[wp_lid][wp_wid] = wp_bid;
-            if (wp_lid>=1){ // ignore the first layer (dummy)
-                map_check[wp_lid][wp_wid] = 1;
-                v_wire_xc.push_back((wp_xhv+wp_xro)/2.);
-                v_wire_yc.push_back((wp_yhv+wp_yro)/2.);
-            }
+            map_theta[wp_lid][wp_wid] = atan(-(wp_xhv-wp_xro)/chamberHL/2); // rotation angle w.r.t the dart plane: read out  plane; positive rotation angle point to -x direction
         }
         else{
             fprintf(stderr,"ERROR: Entry %d in wiremap file, lid = %d wid = %d out of range (%d,%d)!\n",i,wp_lid,wp_wid,NLAY,NCEL);
@@ -255,8 +279,6 @@ int main(int argc, char** argv){
     //===================Prepare output ROOT file============================
     TFile * of = new TFile(Form("../root/i_%d.layer%d",runNo,testlayer)+suffix+".root","RECREATE"); 
     TTree * ot = new TTree("t","t");
-    std::vector<double> * o_dxl= 0;
-    std::vector<double> * o_dxr= 0;
     std::vector<std::vector<double> > * o_fitD = 0;
     double o_tx1[NCAND];
     double o_tx2[NCAND];
@@ -278,6 +300,7 @@ int main(int argc, char** argv){
     ot->Branch("sum",&i_sum);
     ot->Branch("aa",&i_aa);
     // 
+    ot->Branch("driftD",&o_driftD);
     ot->Branch("dxl",&o_dxl);
     ot->Branch("dxr",&o_dxr);
     // after track finding, with different candidates;
@@ -292,6 +315,7 @@ int main(int argc, char** argv){
         std::vector<double> fitDs;
         o_fitD->push_back(fitDs);
     }
+    o_driftD = new std::vector<double>;
     o_dxl = new std::vector<double>;
     o_dxr = new std::vector<double>;
 
@@ -300,35 +324,30 @@ int main(int argc, char** argv){
     int N_found = 0;
     int N_good = 0;
 
-    std::vector<std::vector<int> > v_hit_index;
+    //===================Hit handlers============================
+    std::vector<std::vector<int> > v_layer_ihit; // vectors of indices of hits in each layer
     for (int i = 0; i<NLAY; i++){
         std::vector<int> hits;
-        v_hit_index.push_back(hits);
+        v_layer_ihit.push_back(hits);
     }
-    std::vector<int> v_pick_lid;
-    std::vector<int> v_pick_index;
-    v_pick_index.resize(NLAY);
-    int nHitsgood;
-    int nHitLayers;
-
-    //===================About tracking============================
-    double yup = 623.97007;
-    double ydown = 527.60011;
-    TF1 * f_x = new TF1("f_x","pol1",500,640); // x VS y
-    TGraph * g_x = 0; // x VS y
-    TF1 * f_z = new TF1("f_z","pol1",500,640); // z VS y
-    TGraph * g_z = 0; // z VS y
+    std::vector<int> v_pick_lid; // layer index of each picked layer (for pair check)
+    std::vector<int> pick_ihit; // hit index of each picked hit (for pair check)
+    std::vector<double> pick_wy; // y position of each picked hit pair (for pair check)
+    pick_ihit.resize(NLAY);
+    pick_wy.resize(NLAY);
+    int nHitsgood; // number of good hits
+    int nHitLayers; // number of layers with hit
 
     //===================Track Finding============================
     Long64_t N = c->GetEntries();
-    if (nEventMax&&nEventMax<N) N = nEventMax;
-    for ( int iEntry = 0; iEntry<N; iEntry++){
-        if (iEntry%1000==0) std::cout<<(double)iEntry/N*100<<"%..."<<std::endl;
+    for ( int iEntry = iEntryStart; iEntry<=iEntryStop; iEntry++){
+        if (iEntry%1000==0) std::cout<<iEntry<<std::endl;
         c->GetEntry(iEntry);
         N_trigger++; // triggered event
 
         //========================================================================================================
         // prepare
+        o_driftD->clear();
         o_dxl->clear();
         o_dxr->clear();
         for (int i = 0; i<NCAND; i++){
@@ -336,7 +355,7 @@ int main(int argc, char** argv){
         }
 
         for (int i = 0; i<NLAY; i++){
-            v_hit_index[i].clear();
+            v_layer_ihit[i].clear();
         }
         nHitsgood = 0;
         nHitLayers = 0;
@@ -354,13 +373,17 @@ int main(int argc, char** argv){
             int status; // 1: right; 2: right end; -1: left; -2: left end; 0 out of range
             o_dxl->push_back(t2x(dt,lid,wid,-1,status));
             o_dxr->push_back(t2x(dt,lid,wid,1,status));
-            v_hit_index[lid].push_back(ihit);
-            nHitsgood++;
+            o_driftD->push_back((*o_dxr)[o_dxr->size()-1]);
+            v_layer_ihit[lid].push_back(ihit);
+            if (lid != testlayer) nHitsgood++;
         }
 
         // get number of layers with good hits;
+        printf("#####################################\n");
+        printf("Entry %d\n",iEntry);
         for(int lid = 1; lid<NLAY; lid++){
-            if (v_hit_index[lid].size()>0)
+            printf(" layer %d: %d hits\n",lid,v_layer_ihit[lid].size());
+            if (lid!=testlayer&&v_layer_ihit[lid].size()>0)
                 nHitLayers++;
         }
 
@@ -372,16 +395,20 @@ int main(int argc, char** argv){
         int prelid = -1;
         for(int lid = 1; lid<NLAY-1; lid++){
             if (lid==testlayer||lid+1==testlayer) continue;
-            if (v_hit_index[lid].size()>0&&v_hit_index[lid+1].size()>0){// both layers have hits
+            if (v_layer_ihit[lid].size()>0&&v_layer_ihit[lid+1].size()>0){// both layers have hits
                 if (prelid+1 != lid)v_pick_lid.push_back(lid);
                 v_pick_lid.push_back(lid+1);
                 prelid = lid;
             }
         }
+        for (int ipick = 0; ipick<v_pick_lid.size(); ipick++){
+            printf(" pick layer %d\n",v_pick_lid[ipick]);
+        }
 
         //========================================================================================================
-        // pick up one hit from each layer
-        ChooseHits(0,v_pick_lid,v_pick_index,v_hit_index);
+        // pick up one hit from each layer, and iterate in all combinations
+        int nSelections = 0;
+        ChooseHits(0,v_pick_lid,pick_ihit,pick_wy,v_layer_ihit,nSelections);
 
         ot->Fill();
     }// end of event loop
@@ -394,36 +421,88 @@ int main(int argc, char** argv){
     return 0;
 }
 
-int ChooseHits(int ipick, std::vector<int> & v_pick_lid, std::vector<int> & v_pick_index, std::vector<std::vector<int> > & v_hit_index){
+int ChooseHits(int ipick, std::vector<int> & v_pick_lid, std::vector<int> & pick_ihit, std::vector<double> & pick_wy, std::vector<std::vector<int> > & v_layer_ihit,int & iselection){
     if (ipick == v_pick_lid.size()){ // finished picking hits
-        checkCrossPoints(v_pick_index);
+        printf(" Finished picking selection %d:\n",iselection);
+        checkCrossPoints(v_pick_lid.size(),pick_ihit,pick_wy);
+        iselection++;
     }
     else{
         int lid = v_pick_lid[ipick];
-        for (int ihit = 0; ihit<v_hit_index[lid].size(); ihit++){
-            v_pick_index[ipick] = v_hit_index[lid][ihit];
-            ChooseHits(ipick+1,v_pick_lid,v_pick_index,v_hit_index);
+        for (int i = 0; i<v_layer_ihit[lid].size(); i++){
+            printf(" => pick # %d, layer %d, hit[%d] %d\n",ipick,lid,i,v_layer_ihit[lid][i]);
+            pick_ihit[ipick] = v_layer_ihit[lid][i];
+            ChooseHits(ipick+1,v_pick_lid,pick_ihit,pick_wy,v_layer_ihit,iselection);
         }
     }
     return 0;
 }
 
-int checkCrossPoints(std::vector<int> & v_pick_index){
-    int nPicks = v_pick_index.size();
+int checkCrossPoints(int nPicks,std::vector<int> & pick_ihit, std::vector<double> & pick_wy){
     int ncombi = pow(2,nPicks);
+    printf("  %d picked layers -> %d combinations\n",nPicks,ncombi);
+    updateHitPositions(nPicks, pick_ihit,pick_wy);
     for (int icombi = 0; icombi<ncombi; icombi++){ // each combination corresponds to a unique left/right selection set
-        for (int ipick = 0; ipick<nPicks; ipick++){
-            int ihit = v_pick_index[ipick];
-            int jhit = v_pick_index[ipick+1];
+        printf("     combi %d\n",icombi);
+        for (int ipick = 0; ipick<nPicks-1; ipick++){
+            int ihit = pick_ihit[ipick];
+            int jhit = pick_ihit[ipick+1];
             int lid = (*i_layerID)[ihit];
             int ljd = (*i_layerID)[jhit];
             if (lid+1!=ljd) continue; // not adjacent
             int ilr = (icombi&(1<<ipick))>>ipick;
             int jlr = (icombi&(1<<(ipick+1)))>>(ipick+1);
-            double x,y,z;
-//            getCrossPoint(ihit,jhit,ilr,jlr,x,y,z);
+            printf("      %d: %d,%d(%d) %d,%d(%d)\n",ipick,lid,ihit,ilr,ljd,jhit,jlr);
+            getCrossPoints(ihit,jhit,ilr,jlr,pick_wy[ipick+1]-pick_wy[ipick]);
         }
     }
+}
+
+int updateHitPositions(int nPicks, std::vector<int> & pick_ihit, std::vector<double> & pick_wy){
+    // calculate pick_wy
+    for (int ipick = 0; ipick<nPicks; ipick++){
+        // Get hit information
+        int ihit = pick_ihit[ipick];
+        int lid = (*i_layerID)[ihit];
+        int wid = (*i_wireID)[ihit];
+        double dd = (*o_driftD)[ihit]; // !!! make sure it's mm
+        double wxro = map_x[lid][wid][1];
+        double wyro = map_y[lid][wid][1];
+        double wzro = chamberHL;
+        double wxhv = map_x[lid][wid][0];
+        double wyhv = map_y[lid][wid][0];
+        double wzhv = -chamberHL;
+        // assume wy
+        double wy = (wyro+wyhv)/2.;
+        // get wz by extrapolating the track to wy
+        double zdown = zup + (ydown-yup)*slz;
+        double xdown = xup + (ydown-yup)*slx;
+        double wz = ((yup-wy)*zdown+(wy-ydown)*zup)/(yup-ydown);
+        // get wx according to wz
+        double wx = ((wzro-wz)*wxhv+(wz-wzhv)*wxro)/(wzro-wzhv);
+        // correct wy according to wz
+        wy = ((wzro-wz)*wyhv+(wz-wzhv)*wyro)/(wzro-wzhv);
+        pick_wy[ipick] = wy;
+    }
+}
+
+int getCrossPoints(int ihit,int jhit,int ilr,int jlr,double deltaY){
+    double dd1, dd2;
+    if (ilr) dd1 = (*o_dxr)[ihit]; // right
+    else     dd1 = (*o_dxl)[ihit]; // left
+    if (jlr) dd2 = (*o_dxr)[jhit]; // right
+    else     dd2 = (*o_dxl)[jhit]; // left
+    int lid = (*i_layerID)[ihit];
+    int ljd = (*i_layerID)[jhit];
+    int wid = (*i_wireID)[ihit];
+    int wjd = (*i_wireID)[jhit];
+    double theta1 = map_theta[lid][wid];
+    double theta2 = map_theta[ljd][wjd];
+    double sintheta12 = sin(theta1-theta2);
+    double zc_fix_slx = deltaY*slx/(tan(theta2)-tan(theta1));
+    double xc = mcp_xc[lid][wid][wjd]+dd1*sin(theta2)/(-sintheta12)+dd2*sin(theta1)/sintheta12;
+    double zc = mcp_zc[lid][wid][wjd]+dd1*cos(theta2)/(-sintheta12)+dd2*cos(theta1)/sintheta12+zc_fix_slx;
+    fityz();
 }
 
 int fityz(){
