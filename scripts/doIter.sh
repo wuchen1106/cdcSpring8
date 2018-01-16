1aa85c92abfdebe671d4794492f4973b5dbfca78
wuchen <wuchen1106@gmail.com>
2018-01-16 12:42:01 +0900

    Updated iteration method

--- scripts/doIter.sh
+++ scripts/doIter.sh
@@ -2,15 +2,15 @@
 
 script="submitKEK.sh"
 StartName="Garfield"
-runNo="100002"
-nEvents="90457"
-runName="0111"
+runNo="1012"
+nEvents="493189"
+runName="0115"
 IterStart=1
-IterEnd=3
+IterEnd=10
 layers="4"
 
 geoType=0 # 0 for general; 1 for finger
-inputType=1 # 1 for MC; 0 for data
+inputType=0 # 1 for MC; 0 for data
 workType=0 # 0, fr/l_0; 1, even/odd; -1, even/odd reversed; others, all layers
 nHitsMax=13
 t0shift=0
@@ -35,6 +35,19 @@ do
         name="${runName}.i${iter}"
         prename="${runName}.i${im1}"
     fi
+
+    # one layer or more
+    if [ $iter -gt 3 ]
+    then
+        layers="3 4 5 6"
+    elif [ $iter -gt 6 ]
+        layers="2 3 4 5 6"
+    elif [ $iter -gt 9 ]
+        layers="1 2 3 4 5 6 7"
+    else
+        layers="4"
+    fi
+
     eval $script -g $geoType -i $inputType -w $workType -n $nHitsMax -t $t0shift -a $tmin -z $tmax -s $sumCut -q $aaCut -d $debug $runNo $nEvents $prename $name $layers
 
     for (( i=0; i<3600; i++ )) #3600*10 sec = 10 hours running
@@ -68,5 +81,15 @@ do
     done
     rm t_${runNo}.${name}.*-*.*
     cd ..
+
+#   upgrading wireposition?
+    if [ $iter -gt 2 ]
+    then
+        getOffset $runNo $prename $name $geoType $DEBUG
+        XTTYPE=1 # 1 for offset loading
+    else
+        cp info/wire-position.${runNo}.${prename}.root info/wire-position.${runNo}.${name}.root
+        XTTYPE=2 # 2 for symmetric without offset
+    fi
     getXT $runNo $prename $name $XTTYPE $geoType $SAVEHISTS $inputType $DEBUG
 done
