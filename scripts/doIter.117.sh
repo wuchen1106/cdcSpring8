2c6f3bb752864786dbfa4df74ee0fa2a0a7bba76
wuchen <wuchen1106@gmail.com>
2018-01-27 19:29:52 +0900

    updated scripts/doIter.117.sh with thread cross usage case

--- scripts/doIter.117.sh
+++ scripts/doIter.117.sh
@@ -4,19 +4,21 @@ StartName="original"
 runNo="117"
 runNocon="117"
 nEvents="605460"
-runName="0125"
-IterStart=7
+runName="0126"
+IterStart=15
 IterEnd=50
 layers="4"
 wires=""
 
-geoSetup=1 # 0 for general; 1 for finger
-inputType=0 # 1 for MC; 0 for data
+geoSetup=0 # 0 for general; 1 for finger
+inputType=1 # 1 for MC; 0 for data
 workType=0 # 0, fr/l_0; 1, even/odd; -1, even/odd reversed; others, all layers
 nHitsMax=13
 t0shift=0
+#tmin=-10
+#tmax=340
 tmin=-10
-tmax=600
+tmax=800
 sumCut=-20
 aaCut=20
 debug=-1
@@ -26,11 +28,137 @@ minslz=0 # min slz cut for mean slz value in each sample of events in wiremap ca
 maxslz=0 # min slz cut for mean slz value in each sample of events in wiremap calibration. minslz==maxslz==0 means no cut
 mininx=0 # min inx cut for mean inx value in each sample of events in wiremap calibration. mininx==maxinx==0 means no cut
 maxinx=0 # min inx cut for mean inx value in each sample of events in wiremap calibration. mininx==maxinx==0 means no cut
+maxchi2=1
 scale=1 # move scale*offset on wiremap for fitting in the next round
 XTTYPE=1 # 2 for symmetrical; 1 for offset loading; 0 for no constraints.
 WPTYPE=0 # 0 for changing wiremap; 1 for not changing it;
+UPDATEXT=1
 DEBUG=-1
 SAVEHISTS=0
+
+lastxtfile="xt.117.0126.i11.root"
+
+isReady(){ # make sure this thread is not processing any job (the conf is emtpy)!
+    conf=$1
+    log=$2
+    echo "test" > $conf
+    sleep 11
+    echo "" > $conf
+    if [ -e $log ]
+    then
+        message=`tail -n 1 $log`
+        if [ ! -z "$message" ]
+        then
+            if echo $message | grep -q "Reset this job"
+            then
+                return 0
+            else
+                return 1
+            fi
+        else
+            return 2
+        fi
+    else
+        return 3
+    fi
+}
+
+checkThread(){
+    tempname=$1
+    tlayer=$2
+    iStart=$3
+    iStop=$4
+    conf="$CDCS8WORKING_DIR/Conf/${runNocon}.layer${tlayer}.${iStart}-${iStop}.conf"
+    log="$CDCS8WORKING_DIR/Conf/${runNocon}.layer${tlayer}.${iStart}-${iStop}.log"
+    if [ ! -e $conf ]
+    then
+        echo "    ERROR! configure file doesn't exist! $conf"
+        return 6
+    fi
+    configure=`cat $conf`
+    if [ ! -z "$configure" ] # thread with configure
+    then
+        if [ ! -e $log ] # but no log file?
+        then
+            sleep 3 # wait
+            if [ ! -e $log ] # really no log file!
+            then
+                echo "    ERROR! configure file not empty but no log file! $conf"
+                echo "        $configure"
+                return 4 # job dead!?
+            fi
+        fi
+        NPARAS=`echo $configure | gawk '{print NF;}'`
+        if [ $NPARAS -eq 16 ] # configure format is correct
+        then
+            trunno=`echo $configure | gawk '{print $1;}'`
+            ttlayer=`echo $configure | gawk '{print $2;}'`
+            ttempname=`echo $configure | gawk '{print $4;}'`
+            tiStart=`echo $configure | gawk '{print $12;}'`
+            tiStop=`echo $configure | gawk '{print $13;}'`
+            if [ $trunno == $runNo -a $ttempname == $tempname -a $ttlayer == $tlayer -a $tiStart == $iStart -a $tiStop == $iStop ]
+            then
+                return 1 # under processing
+            else
+                return 2 # used by others
+            fi
+        else # configure format is wrong but not reset?
+            echo "    ERROR! wrong format in $conf"
+            echo "        $configure"
+            if [ -e $log ]; then tail $log; fi
+            return 5 # job dead!?
+        fi
+    else  # thread with no configure, probably not processing any job
+        isReady $conf $log
+        if [ $? -eq 0 ]
+        then
+            return 0 # ready to process
+        else
+            return 3 # Waiting in line
+        fi
+    fi
+}
+
+findVacentThread(){
+    for (( i=0; i<3600; i++ )) #3600*10 sec = 10 hours running
+    do
+        for tlayer in 4 5 3 6 2 7 1 8;
+        do
+            for (( j=0; j<nEvents; j+=5000 ))
+            do
+                iStart=$j
+                iStop=$((j+4999))
+                if (( iStop>=nEvents ))
+                then
+                    iStop=$((nEvents-1))
+                fi
+                conf="$CDCS8WORKING_DIR/Conf/${runNocon}.layer${tlayer}.${iStart}-${iStop}.conf"
+                log="$CDCS8WORKING_DIR/Conf/${runNocon}.layer${tlayer}.${iStart}-${iStop}.log"
+                if [ ! -e $conf ]
+                then
+                    continue
+                fi
+                configure=`cat $conf`
+                if [ -z "$configure" ] # thread with no configure, probably not processing any job
+                then
+                    isReady $conf $log
+                    if [ $? -eq 0 ]
+                    then
+                        echo $conf
+                        return 0 # ready to process
+                    else # waiting in line
+                        continue
+                    fi
+                else # dealing with other jobs
+                    continue
+                fi
+            done
+        done
+        sleep 10
+    done
+    echo "Still cannot find a vacent thread!"
+    exit 1
+}
 
 for (( iter=IterStart; iter<=IterEnd; iter++ ))
 do
@@ -59,6 +187,7 @@ do
     then
         layers="1 2 3 4 5 6 7 8"
         WPTYPE=1 # 1 for changing wiremap
+        UPDATEXT=1
         wires=""
         stepSize="0" # no step size limit
         scale="0.5"
@@ -70,6 +199,7 @@ do
     then
         layers="3 4 5 6"
         WPTYPE=1 # 1 for changing wiremap
+        UPDATEXT=1
         wires=""
         stepSize="0" # no step size limit
         scale="0.5"
@@ -81,6 +211,7 @@ do
     then
         layers="1 2 7 8"
         WPTYPE=1 # 1 for changing wiremap
+        UPDATEXT=0
         wires=""
         stepSize="0" # no step size limit
         scale="0.5"
@@ -92,6 +223,7 @@ do
     then
         layers="8"
         WPTYPE=1 # 1 for changing wiremap
+        UPDATEXT=0
         wires=""
         stepSize="0" # no step size limit
         scale="0.5"
@@ -103,6 +235,7 @@ do
     then
         layers="1"
         WPTYPE=1 # 1 for changing wiremap
+        UPDATEXT=0
         wires=""
         stepSize="0" # no step size limit
         scale="0.5"
@@ -114,6 +247,7 @@ do
     then
         layers="7"
         WPTYPE=1 # 1 for changing wiremap
+        UPDATEXT=0
         wires=""
         stepSize="0" # no step size limit
         scale="0.5"
@@ -125,6 +259,7 @@ do
     then
         layers="2"
         WPTYPE=1 # 1 for changing wiremap
+        UPDATEXT=0
         wires=""
         stepSize="0" # no step size limit
         scale="0.5"
@@ -136,6 +271,7 @@ do
     then
         layers="3 4 5 6"
         WPTYPE=1 # 1 for changing wiremap
+        UPDATEXT=1
         wires=""
         stepSize="0" # no step size limit
         scale="0.5"
@@ -147,6 +283,7 @@ do
     then
         layers="6"
         WPTYPE=1 # 1 for changing wiremap
+        UPDATEXT=0
         wires=""
         stepSize="0" # no step size limit
         scale="0.5"
@@ -158,6 +295,7 @@ do
     then
         layers="3"
         WPTYPE=1 # 1 for changing wiremap
+        UPDATEXT=0
         wires=""
         stepSize="0" # no step size limit
         scale="0.5"
@@ -169,6 +307,7 @@ do
     then
         layers="5"
         WPTYPE=1 # 1 for changing wiremap
+        UPDATEXT=0
         wires=""
         stepSize="0" # no step size limit
         scale="0.5"
@@ -180,6 +319,7 @@ do
     then
         layers="4"
         WPTYPE=1 # 1 for changing wiremap
+        UPDATEXT=1
         wires=""
         stepSize="0" # no step size limit
         scale="0.5"
@@ -190,6 +330,7 @@ do
     else
         layers="4"
         WPTYPE=0 # 0 for not changing wiremap
+        UPDATEXT=1
     fi
 
     Njobs=0
@@ -197,26 +338,75 @@ do
     do
         for (( j=0; j<nEvents; j+=5000 ))
         do
-            ((Njobs++))
             iEntryStart=$j
             iEntryStop=$((j+4999))
             if (( iEntryStop>=nEvents ))
             then
                 iEntryStop=$((nEvents-1))
             fi
-            temprunname="${currunname}.$iEntryStart-$iEntryStop"
+            jobname="${runNo}.${currunname}.$iEntryStart-$iEntryStop.layer${testlayer}"
+            ((Njobs++))
+            echo "checking job \"$jobname\""
+            file="root/t_${jobname}.log"
+            if [ -e $file ]
+            then # log file already exists??
+                if tail -n 3 $file | grep -q "Good Events" # finished
+                then
+                    echo "Job $jobname already finished!"
+                    continue # no need to work on it
+                else  # probably still under process
+                    echo "Job $jobname is running by someone else!"
+                    tail -n 1 $file
+                    continue # don't know who is running it but ignore this job anyway
+                fi
+            else
+                echo "$file doesn't exist, so generate a new job!"
+            fi
             theConf="$CDCS8WORKING_DIR/Conf/${runNocon}.layer${testlayer}.${iEntryStart}-${iEntryStop}.conf"
-            echo "$runNo $testlayer $prerunname $temprunname $nHitsMax $t0shift $tmin $tmax $geoSetup $sumCut $aaCut $iEntryStart $iEntryStop $workType $inputType $debug" > $theConf # send the trigger info to the job
+            temprunname="${currunname}.$iEntryStart-$iEntryStop"
+            tempconfig="$runNo $testlayer $prerunname $temprunname $nHitsMax $t0shift $tmin $tmax $geoSetup $sumCut $aaCut $iEntryStart $iEntryStop $workType $inputType $debug"
+            checkThread ${temprunname} ${testlayer} ${iEntryStart} ${iEntryStop}
+            result=$?
+            if [ $result -eq 0 ] # thread is ready
+            then # go with this job
+                echo "Job $jobname is ready"
+                echo "$tempconfig" > $theConf # send the trigger info to the job
+            elif [ $result -eq 1 ] # thread is running this job
+            then # do nothing
+                echo "Job $jobname is already running!"
+                tail $theConf
+            elif [ $result -eq 2 ] # thread is used by others
+            then # notify and find a job which is emtpy
+                echo "Job $jobname is used by others!"
+                tail $theConf
+                echo "    will find another job to use..."
+                theConf=`findVacentThread`
+                echo "    found new job available $theConf"
+                echo "$tempconfig" > $theConf # send the trigger info to the job
+            elif [ $result -eq 3 ] # thread is still waiting
+            then # notify and find a job which is emtpy
+                echo "Job $jobname is still waiting in queue!"
+                echo "    will find another job to use..."
+                theConf=`findVacentThread`
+                echo "    found new job available $theConf"
+                echo "$tempconfig" > $theConf # send the trigger info to the job
+            else # error with this thread
+                echo "Job $jobname is dead?"
+                echo "    will find another job to use..."
+                theConf=`findVacentThread`
+                echo "    found new job available $theConf"
+                echo "$tempconfig" > $theConf # send the trigger info to the job
+            fi
         done
     done
     echo "Iteration $iter, $Njobs to be finished!"
 
+    allfinished=false
     for (( i=0; i<3600; i++ )) #3600*10 sec = 10 hours running
     do
-        sleep 10
         echo -n "$i "
         finished=true
-        thefile="temp"
+        thefile=""
         NjobsFinished=0
         for file in root/t_${runNo}.${currunname}.*.log
         do
@@ -235,13 +425,25 @@ do
         if $finished
         then
             echo "Iteration $iter finished"
+            allfinished=true
             break
         else
             echo -n "$NjobsFinished/$Njobs jobs finihsed, "
-            echo -n $thefile:
-            tail -n 1 $thefile
+            if [ -z $thefile ]
+            then
+                echo ""
+            else
+                echo -n $thefile:
+                tail -n 1 $thefile
+            fi
         fi
+        sleep 10
     done
+    if [ ! $allfinished ]
+    then
+        echo "Still not finished!"
+        exit 1
+    fi
 
     cd root/
     pids=""
@@ -251,7 +453,7 @@ do
         pids+=" $!"
     done
     wait $pids || { echo "there were errors" >&2; exit 1; }
-    rm t_${runNo}.${currunname}.*-*.*
+    rm -f t_${runNo}.${currunname}.*-*.*
     cd ..
 
 #   upgrading wireposition?
@@ -261,6 +463,18 @@ do
         ln -s wire-position.${runNo}.${StartName}.root wire-position.${runNo}.${currunname}.root
         cd ..
     fi
-    getOffset $runNo $prerunname $currunname $geoSetup $WPTYPE $scale $stepSize $minslz $maxslz $mininx $maxinx $DEBUG $wires
-    getXT $runNo $prerunname $currunname $XTTYPE $geoSetup $SAVEHISTS $inputType $DEBUG
+    getOffset $runNo $prerunname $currunname $geoSetup $WPTYPE $scale $stepSize $minslz $maxslz $mininx $maxinx $maxchi2 $DEBUG $wires
+    if [ ! $UPDATEXT -eq 1 ] # ! 1 for not updating xt
+    then
+        if [ -z $lastxtfile ]
+        then
+            lastxtfile=xt.${runNo}.${prerunname}.root
+        fi
+        cd info
+        ln -s $lastxtfile xt.${runNo}.${currunname}.root
+        cd ..
+    else
+        getXT $runNo $prerunname $currunname $XTTYPE $geoSetup $SAVEHISTS $inputType $maxchi2 $DEBUG
+        lastxtfile=xt.${runNo}.${currunname}.root
+    fi
 done
