7decb7dceb9df8efb7281f7ef6145732ba5d04c8
wuchen <wuchen1106@gmail.com>
2018-05-09 17:57:58 +0900

    Added two more scripts to check MC resolution with BestRuns list and to translate the log file from getResEff

---
+++ scripts/loopBestRuns.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+bestRuns="bestRuns"
+
+NRUNS=`cat $CDCS8WORKING_DIR/$bestRuns | wc -l`
+for (( i=1; i<=$NRUNS; i++ ))
+do
+    runNo=`gawk -v gawk_keyword=$i 'BEGIN{i=1;}{if (i==gawk_keyword) print $3; i++;}' $bestRuns`
+    runName=`gawk -v gawk_keyword=$i 'BEGIN{i=1;}{if (i==gawk_keyword) print $4"."$5; i++;}' $bestRuns`
+    aaCut=`echo $runName | sed 's/.*a\(\w*\)n\(\w*\)\.i\w*/\1/'`
+    CHI2MAX=2
+    NMAX=`echo $runName |  sed 's/.*a\(\w*\)n\(\w*\)\.i\w*/\2/'`
+    SLZMAX="0.1"
+
+    found=false;
+    for setuplist in $CDCS8WORKING_DIR/Input/list.C*.*;
+    do
+        for rid in `cat $setuplist`
+        do
+            if [ $rid == $runNo ]
+            then
+                found=true
+                break;
+            fi
+        done
+        if $found
+        then
+            break;
+        fi
+    done
+    setup=`echo $setuplist | sed 's/.*\/list\.\(.*\)/\1/'`
+    GAS=`echo $setup | sed 's/\(\w*\)\.\(\w*\)/\1/'`
+    HV=`echo $setup | sed 's/\(\w*\)\.\(\w*\)/\2/'`
+    if [ $GAS = "C2H6" -a $HV -lt 2000 ]
+    then
+        NMIN=5
+    elif [ $GAS = "C4H10" -a $HV -lt 1650 ]
+    then
+        NMIN=5
+    elif [ $GAS = "CH4" -a $HV -lt 1850 ]
+    then
+        NMIN=5
+    else
+        NMIN=7
+    fi
+
+    doIter.res.sh $runNo 0426resxAV 0 400 1 5 $runName $aaCut $CHI2MAX $NMIN $NMAX $SLZMAX > $CDCS8WORKING_DIR/res.$runNo.0426resxAV 2>&1 &
+    pids="$!"
+    wait $pids || { echo "there were errors in combining $runNo $currunname $ilayer" >&2; exit 1; }
+done
---
+++ scripts/trans.analog.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+if [ $# -lt 2 ]
+then
+    exit 0
+fi
+logfile=$1
+output=$2
+temp=/tmp/trans.analog
+
+title=`grep "^=>" $logfile | tail -n 1 | sed 's/=>//'`
+echo "$title avEtrackx0 avEtrackd0 avresInix0 avresInid0 avEtrackx avEtrackd avresInix avresInid" > $output
+
+touch $temp
+grep "^==>" $logfile |sed "s/==>//g"| gawk '{if ($5==0) print $0;}' | sort -k 4 -n | sort -k 3 >> $temp
+grep "^==>" $logfile |sed "s/==>//g"| gawk '{if ($5==1) print $0;}' | sort -k 4 -n | sort -k 3 >> $temp
+grep "^==>" $logfile |sed "s/==>//g"| gawk '{if ($5==2) print $0;}' | sort -k 4 -n | sort -k 3 >> $temp
+NRUNS=`cat $temp| wc -l`
+for (( i=1; i<=$NRUNS; i++ ))
+do
+    runNo=`gawk -v gawk_keyword=$i 'BEGIN{i=1;}{if (i==gawk_keyword) print $1; i++;}' $temp`
+    line=`gawk -v gawk_keyword=$i 'BEGIN{i=1;}{if (i==gawk_keyword) print $0; i++;}' $temp`
+    resfile=res.$runNo.*
+    if [ ! -e $resfile ]
+    then
+        echo "Cannot find $resfile!"
+        continue
+    fi
+    avEtrackx0=`cat $resfile | grep "avEtrackx" | tail -n 2 | sed "s/,//" | gawk 'BEGIN{i=1;}{if (i==1) {print $2} i++;}'`
+    avEtrackd0=`cat $resfile | grep "avEtrackx" | tail -n 2 | sed "s/,//" | gawk 'BEGIN{i=1;}{if (i==1) {print $4} i++;}'`
+    avEtrackx=`cat $resfile | grep "avEtrackx" | tail -n 2 | sed "s/,//" | gawk 'BEGIN{i=1;}{if (i==2) {print $2} i++;}'`
+    avEtrackd=`cat $resfile | grep "avEtrackx" | tail -n 2 | sed "s/,//" | gawk 'BEGIN{i=1;}{if (i==2) {print $4} i++;}'`
+    avresInix0=`cat $resfile | grep "avresInix" | tail -n 2 | sed "s/,//" | gawk 'BEGIN{i=1;}{if (i==1) {print $2} i++;}'`
+    avresInid0=`cat $resfile | grep "avresInix" | tail -n 2 | sed "s/,//" | gawk 'BEGIN{i=1;}{if (i==1) {print $4} i++;}'`
+    avresInix=`cat $resfile | grep "avresInix" | tail -n 2 | sed "s/,//" | gawk 'BEGIN{i=1;}{if (i==2) {print $2} i++;}'`
+    avresInid=`cat $resfile | grep "avresInix" | tail -n 2 | sed "s/,//" | gawk 'BEGIN{i=1;}{if (i==2) {print $4} i++;}'`
+    echo "$line $avEtrackx0 $avEtrackd0 $avresInix0 $avresInid0 $avEtrackx $avEtrackd $avresInix $avresInid" >> $output
+done
+
+rm $temp
