1f0309ab722da32bad4d0cc9560353b2ef50264e
wuchen <wuchen1106@gmail.com>
2019-09-06 20:20:26 +0900

    Updated anaBump.C

--- macros/anaBump.C
+++ macros/anaBump.C
@@ -18,59 +18,66 @@
     int NBinXZ = 256;
     TH2D * hYZ[NLAY];
     TH2D * hYX[NLAY];
-    TH3D * hYXZ = new TH3D("hYXZ","",NBinXZ,Xmin,Xmax,NBinXZ,Zmin,Zmax,NBinY,Ymin,Ymax);
-    hYXZ->GetXaxis()->SetTitle("X");
-    hYXZ->GetYaxis()->SetTitle("Y");
-    hYXZ->GetZaxis()->SetTitle("Z");
+    TH2D * hXZ[NLAY];
     for (int i = 0; i<NLAY; i++){
         hYZ[i] = new TH2D(Form("hYZ%d",i),Form("Beam in Layer %d with DOCA %.0f~%.0f mm",i,FDmin[i],FDmax[i]),NBinXZ,Zmin,Zmax,NBinY,Ymin,Ymax);
         hYX[i] = new TH2D(Form("hYX%d",i),Form("Beam in Layer %d with DOCA %.0f~%.0f mm",i,FDmin[i],FDmax[i]),NBinXZ,Xmin,Xmax,NBinY,Ymin,Ymax);
+        hXZ[i] = new TH2D(Form("hXZ%d",i),Form("Beam in Layer %d with DOCA %.0f~%.0f mm",i,FDmin[i],FDmax[i]),NBinXZ,Xmin,Xmax,NBinXZ,Zmin,Zmax);
     }
 
     int Iteration = 5;
-    TCanvas * canv3D = new TCanvas("canv3D","",512,512);
-    canv3D->cd();
-    hYXZ->Draw();
-    for (int iLayer = 4; iLayer<=5; iLayer++){
-        TChain * ichain = new TChain("t","t");
-        ichain->Add(Form("root/ana/ana_58.0902xt053l4.i%d.layer%d.root",Iteration,iLayer));
-        int    nHitsS;
-        double chi2;
-        int    theWid0;
-        double theFD0;
-        double slx;
-        double inx;
-        double slz;
-        double inz;
-        ichain->SetBranchAddress("chi2",&chi2);
-        ichain->SetBranchAddress("nHitsS",&nHitsS);
-        ichain->SetBranchAddress("theWid0",&theWid0);
-        ichain->SetBranchAddress("theFD0",&theFD0);
-        ichain->SetBranchAddress("slx",&slx);
-        ichain->SetBranchAddress("inx",&inx);
-        ichain->SetBranchAddress("slz",&slz);
-        ichain->SetBranchAddress("inz",&inz);
-        canv3D->cd();
-        Long64_t nEntries = ichain->GetEntries();
-        for (Long64_t iEntry = 0; iEntry<10000; iEntry++){
-            ichain->GetEntry(iEntry);
-            if (chi2>2||nHitsS<7||theFD0<FDmin[iLayer]||theFD0>FDmax[iLayer]||theWid0!=WireID[iLayer]) continue;
-            for (int iBin = 1; iBin<NBinY; iBin++){
-                double y = Ymin+(Ymax-Ymin)/NBinY*(iBin-0.5);
-                double x = inx+(y-Yref)*slx;
-                double z = inz+(y-Yref)*slz;
-                hYZ[iLayer]->Fill(z,y);
-                hYX[iLayer]->Fill(x,y);
+    TChain * ichain = new TChain("t","t");
+    ichain->Add("root/ana/ana_58.0902xt053l4.i5.layer4.root");
+    int    nHitsS;
+    double chi2;
+    double slx;
+    double inx;
+    double slz;
+    double inz;
+    std::vector<double> * fitD = 0;
+    std::vector<int> * layerID = 0;
+    std::vector<int> * wireID = 0;
+    ichain->SetBranchAddress("chi2",&chi2);
+    ichain->SetBranchAddress("nHitsS",&nHitsS);
+    ichain->SetBranchAddress("fitD",&fitD);
+    ichain->SetBranchAddress("layerID",&layerID);
+    ichain->SetBranchAddress("wireID",&wireID);
+    ichain->SetBranchAddress("slx",&slx);
+    ichain->SetBranchAddress("inx",&inx);
+    ichain->SetBranchAddress("slz",&slz);
+    ichain->SetBranchAddress("inz",&inz);
+    Long64_t nEntries = ichain->GetEntries();
+    for (Long64_t iEntry = 0; iEntry<10000; iEntry++){
+        ichain->GetEntry(iEntry);
+        for (int iLayer = 1; iLayer<NLAY; iLayer++){
+            for (int iHit = 0; iHit<fitD->size(); iHit++){
+                //if (chi2>2||nHitsS<7||fitD->at(iHit)<FDmin[iLayer]||fitD->at(iHit)>FDmax[iLayer]||layerID->at(iHit)!=iLayer||wireID->at(iHit)!=WireID[iLayer]) continue;
+                if (chi2>2||nHitsS<7||fitD->at(iHit)<FDmin[iLayer]||fitD->at(iHit)>FDmax[iLayer]||layerID->at(iHit)!=iLayer) continue;
+                int preBinXZ = -1;
+                for (int iBin = 1; iBin<NBinY; iBin++){
+                    double y = Ymin+(Ymax-Ymin)/NBinY*(iBin-0.5);
+                    double x = inx+(y-Yref)*slx;
+                    double z = inz+(y-Yref)*slz;
+                    int binXZ = hXZ[iLayer]->FindBin(x,z);
+                    hYZ[iLayer]->Fill(z,y);
+                    hYX[iLayer]->Fill(x,y);
+                    if (binXZ!=preBinXZ) hXZ[iLayer]->Fill(x,z);
+                    preBinXZ = binXZ;
+                }
+                break;
             }
-            TPolyLine3D * line = new TPolyLine3D(2);
-            line->SetPoint(0,inx,inz,Yref);
-            line->SetPoint(1,inx+(Ymin-Yref)*slx,inz+(Ymin-Yref)*slz,Ymin);
-            line->SetLineColor(color[iLayer]);
-            line->Draw();
         }
-        TCanvas * canv = new TCanvas(Form("canv%d",iLayer),"",1024,512);
-        canv->Divide(2,1);
-        canv->cd(1); hYX[iLayer]->Draw("COLZ");
-        canv->cd(2); hYZ[iLayer]->Draw("COLZ");
+    }
+    TCanvas * canvYX = new TCanvas("canvYX","",1024,512);
+    TCanvas * canvYZ = new TCanvas("canvYZ","",1024,512);
+    TCanvas * canvXZ = new TCanvas("canvXZ","",1024,512);
+    canvYX->Divide(4,2);
+    canvYZ->Divide(4,2);
+    canvXZ->Divide(4,2);
+    for (int iLayer = 1; iLayer<NLAY; iLayer++){
+        int i = ((iLayer+1)%2)*4+(iLayer+1)/2;
+        canvYX->cd(i); hYX[iLayer]->Draw("COLZ");
+        canvYZ->cd(i); hYZ[iLayer]->Draw("COLZ");
+        canvXZ->cd(i); hXZ[iLayer]->Draw("COLZ");
     }
 }
