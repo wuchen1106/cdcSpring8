78e255a6f5315d6cf0f4f0a67041d7ffca750fcc
wuchen <wuchen1106@gmail.com>
2019-09-07 18:49:55 +0900

    Updated anaBump.C

--- macros/anaBump.C
+++ macros/anaBump.C
@@ -19,10 +19,12 @@
     TH2D * hYZ[NLAY];
     TH2D * hYX[NLAY];
     TH2D * hXZ[NLAY];
+    TH2D * hLD[NLAY];
     for (int i = 0; i<NLAY; i++){
         hYZ[i] = new TH2D(Form("hYZ%d",i),Form("Beam in Layer %d with DOCA %.0f~%.0f mm",i,FDmin[i],FDmax[i]),NBinXZ,Zmin,Zmax,NBinY,Ymin,Ymax);
         hYX[i] = new TH2D(Form("hYX%d",i),Form("Beam in Layer %d with DOCA %.0f~%.0f mm",i,FDmin[i],FDmax[i]),NBinXZ,Xmin,Xmax,NBinY,Ymin,Ymax);
         hXZ[i] = new TH2D(Form("hXZ%d",i),Form("Beam in Layer %d with DOCA %.0f~%.0f mm",i,FDmin[i],FDmax[i]),NBinXZ,Xmin,Xmax,NBinXZ,Zmin,Zmax);
+        hLD[i] = new TH2D(Form("hLD%d",i),Form("Beam in Layer %d with DOCA %.0f~%.0f mm",i,FDmin[i],FDmax[i]),NBinXZ,-10,10,NLAY,0,NLAY);
     }
 
     int Iteration = 5;
@@ -46,13 +48,20 @@
     ichain->SetBranchAddress("inx",&inx);
     ichain->SetBranchAddress("slz",&slz);
     ichain->SetBranchAddress("inz",&inz);
+    double minFD[NLAY];
     Long64_t nEntries = ichain->GetEntries();
-    for (Long64_t iEntry = 0; iEntry<10000; iEntry++){
+    for (Long64_t iEntry = 0; iEntry<nEntries; iEntry++){
         ichain->GetEntry(iEntry);
         for (int iLayer = 1; iLayer<NLAY; iLayer++){
+            for (int jLayer = 1; jLayer<NLAY; jLayer++){
+                minFD[jLayer] = 10;
+            }
+            bool found = false;
             for (int iHit = 0; iHit<fitD->size(); iHit++){
                 //if (chi2>2||nHitsS<7||fitD->at(iHit)<FDmin[iLayer]||fitD->at(iHit)>FDmax[iLayer]||layerID->at(iHit)!=iLayer||wireID->at(iHit)!=WireID[iLayer]) continue;
-                if (chi2>2||nHitsS<7||fitD->at(iHit)<FDmin[iLayer]||fitD->at(iHit)>FDmax[iLayer]||layerID->at(iHit)!=iLayer) continue;
+                if (chi2>2||nHitsS<7) continue;
+                if (fabs(minFD[layerID->at(iHit)])>fabs(fitD->at(iHit))) minFD[layerID->at(iHit)] = fitD->at(iHit);
+                if(fitD->at(iHit)<FDmin[iLayer]||fitD->at(iHit)>FDmax[iLayer]||layerID->at(iHit)!=iLayer) continue;
                 int preBinXZ = -1;
                 for (int iBin = 1; iBin<NBinY; iBin++){
                     double y = Ymin+(Ymax-Ymin)/NBinY*(iBin-0.5);
@@ -63,21 +72,29 @@
                     hYX[iLayer]->Fill(x,y);
                     if (binXZ!=preBinXZ) hXZ[iLayer]->Fill(x,z);
                     preBinXZ = binXZ;
+                    found = true;
                 }
-                break;
+            }
+            if (found){
+                for (int jLayer = 1; jLayer<NLAY; jLayer++){
+                    hLD[iLayer]->Fill(minFD[jLayer],jLayer);
+                }
             }
         }
     }
     TCanvas * canvYX = new TCanvas("canvYX","",1024,512);
     TCanvas * canvYZ = new TCanvas("canvYZ","",1024,512);
     TCanvas * canvXZ = new TCanvas("canvXZ","",1024,512);
+    TCanvas * canvLD = new TCanvas("canvLD","",1024,512);
     canvYX->Divide(4,2);
     canvYZ->Divide(4,2);
     canvXZ->Divide(4,2);
+    canvLD->Divide(4,2);
     for (int iLayer = 1; iLayer<NLAY; iLayer++){
         int i = ((iLayer+1)%2)*4+(iLayer+1)/2;
-        canvYX->cd(i); hYX[iLayer]->Draw("COLZ");
-        canvYZ->cd(i); hYZ[iLayer]->Draw("COLZ");
-        canvXZ->cd(i); hXZ[iLayer]->Draw("COLZ");
+        canvYX->cd(i); gPad->SetGridx(1); gPad->SetGridy(1); hYX[iLayer]->Draw("COLZ");
+        canvYZ->cd(i); gPad->SetGridx(1); gPad->SetGridy(1); hYZ[iLayer]->Draw("COLZ");
+        canvXZ->cd(i); gPad->SetGridx(1); gPad->SetGridy(1); hXZ[iLayer]->Draw("COLZ");
+        canvLD->cd(i); gPad->SetGridx(1); gPad->SetGridy(1); hLD[iLayer]->Draw("COLZ");
     }
 }
