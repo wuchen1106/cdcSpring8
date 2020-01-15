34e6fbda75ff08d0e65d99dced1b410be1c72017
wuchen <wuchen1106@gmail.com>
2020-01-16 02:30:21 +0900

    Avoid skipping too many hits in the recursive selection loop

--- src/Tracker.cxx
+++ src/Tracker.cxx
@@ -219,8 +219,10 @@ int Tracker::tracking(int iLayer,size_t & iselection){
     else{
         for (int i = -1; i<(int)hitLayerIndexMap->at(iLayer)->size(); i++){ // -1 means don't pick up any hit in this layer 
             if (i==-1){
-                MyNamedVerbose("Tracking",Form(" => skip layer %d",iLayer));
-                tracking(iLayer+1,iselection);
+                if (NLAY-iLayer-1+currentTrack3D.hitIndexSelected.size()-currentTrack3D.nPars()>=1){
+                    MyNamedVerbose("Tracking",Form(" => skip layer %d",iLayer));
+                    tracking(iLayer+1,iselection);
+                }
             }
             else{
                 int ihit = hitLayerIndexMap->at(iLayer)->at(i);
